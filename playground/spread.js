e2788249acc91b1346338cc70661527da3f9a78c
Pablo Pareja <pabloparejatobes@gmail.com>
2017-02-16 12:14:00 +0100

    spread operator tests

---
+++ playground/spread.js
@@ -0,0 +1,30 @@
+// function add (a, b){
+//   return a + b;
+// }
+// console.log(add(3,1));
+//
+// var toAdd = [9,5];
+// console.log(add(...toAdd));
+
+// var groupA = ['Jen', 'Cory'];
+// var groupB = ['Vikram'];
+// var final = [3, ...groupA, ...groupB];
+//
+// console.log(final);
+
+var person = ['Andrew', 25];
+var personTwo = ['Jen', 29];
+
+function greeting(name, age){
+  console.log("Hi " + name + ", you are " + age);
+}
+
+greeting(...person);
+greeting(...personTwo);
+
+var names = ['Mike', 'Ben'];
+var final = [...names, 'Andrew'];
+
+final.forEach(function (name){
+  console.log('Hi ' + name);
+});
