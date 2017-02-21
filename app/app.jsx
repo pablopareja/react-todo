b291cdd7f69a0418f8e2d68a891367d30aa9f42f
Pablo Pareja <pabloparejatobes@gmail.com>
2017-02-21 13:10:33 +0100

    Add Redux store

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,17 @@ $(document).foundation();
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Walk the dog'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 // App css
 require('style!css!sass!applicationStyles')
 
