273de0752598de9332e6842dbc7ccc8a87795367
Pablo Pareja <pabloparejatobes@gmail.com>
2017-02-22 15:13:36 +0100

    Refactor app to work with local storage

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -56,5 +56,23 @@ describe('Reducers', () => {
 
     });
 
+    it('should add existing todos', () => {
+      var todos = [{
+        id: '111',
+        text: 'anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 33000
+      }]
+      var action = {
+        type: 'ADD_TODOS',
+        todos: todos
+      }
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0]).toEqual(todos[0]);
+    });
+
   });
 });
